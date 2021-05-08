ea215a275c948e1c2b893df05a08cf81bcd685dd
vignesh <vigneshvenkat22@gmail.com>
2021-05-08 19:12:01 +0530

    second commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask ,render_template, request
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.ensemble import BaggingClassifier
+
+
+app = Flask(__name__)
+
+
+model= pickle.load(open('model.pkl','rb'))
+
+# Home Route
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+# prediction
+@app.route('/predict',methods=['POST'])
+def predict():
+	int_feature = [float(x) for x in request.form.values()]
+	final_features = [np.array(int_feature)]
+	prediction = model.predict(final_features)
+	output = prediction[0]
+	x = "The Severity of the accident could be "
+	y = output.replace("_"," ")
+	return render_template('index.html',text = x ,predictiontext= y)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
